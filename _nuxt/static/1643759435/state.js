f1ca31f054447b080eec53ff188d36aa9d77e55e
pedro <pedrocarvalho@consolidesuamarca.com.br>
2022-02-01 20:50:52 -0300

    Updates

---
+++ _nuxt/static/1643759435/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-github-pages\u002F_nuxt\u002Fstatic\u002F1643759435",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-github-pages\u002F",assetsPath:"\u002Fnuxt-github-pages\u002F_nuxt\u002F",cdnURL:a}}}}(null));

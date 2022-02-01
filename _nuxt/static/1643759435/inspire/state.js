625cc0ccf71518aa4999b8a9bae0bd02b7282d04
pedro <pedrocarvalho@consolidesuamarca.com.br>
2022-02-01 20:59:15 -0300

    Updates

--- _nuxt/static/1643759435/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-github-pages\u002F_nuxt\u002Fstatic\u002F1643759435",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fnuxt-github-pages\u002F",assetsPath:"\u002Fnuxt-github-pages\u002F_nuxt\u002F",cdnURL:a}}}}(null));

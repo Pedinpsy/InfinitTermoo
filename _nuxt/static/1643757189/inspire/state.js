09c796c76371dc0ea45c5fed53d59137e51c8f55
pedro <pedrocarvalho@consolidesuamarca.com.br>
2022-02-01 20:13:50 -0300

    Updates

---
+++ _nuxt/static/1643757189/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FInfinitTermoo\u002F_nuxt\u002Fstatic\u002F1643757189",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002FInfinitTermoo\u002F",assetsPath:"\u002FInfinitTermoo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
